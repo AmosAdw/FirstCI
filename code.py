11a62ffe233d18c0f9a354372446437b67eb4059
AmosAdw <ada@iokeo.com>
2023-05-17 17:06:49 +0100

    COMMIT MESSAGE 3

--- code.py
+++ code.py
@@ -1,3 +1,12 @@
 print("Hello world")
 name = input("Entrez votre nom : ")
 print("Bonjour,", name)
+print("Let's play a game")
+choice= input("Enter y to accept and n to decline")
+choice=str(choice)
+if choice=="y":
+    print("yes! Let's play")
+else if choice=="n":
+    print("Sh****t!")
+else:
+    print("Enter y or n")
